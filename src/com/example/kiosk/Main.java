16a9b9df5caaa1b1b57ff96e9868adfe539aeeda
kimminsung <shiwing15@naver.com>
2025-04-25 15:48:10 +0900

    feat(main) 메뉴 리스트 저장, 출력 기능 구현

--- src/com/example/kiosk/Main.java
+++ src/com/example/kiosk/Main.java
@@ -1,22 +1,36 @@
 package com.example.kiosk;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("[ SHAKESHACK MENU ]");
-        System.out.println("1. ShackBurger    | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-        System.out.println("2. SmokeShack     | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-        System.out.println("3. CheeseBurger   | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-        System.out.println("4. Hamburger      | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
-        System.out.println("0. 종료            | 종료   |");
-        Scanner scanner = new Scanner(System.in);
-        int i = scanner.nextInt();
+        List<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("CheeseBurger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
-        if(i==0){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("[ SHAKESHACK MENU ]");
+        for (int i = 0; i < menuItems.size(); i++){
+            MenuItem menuItem = menuItems.get(i);
+            System.out.println(i+1 + "." + menuItem.getName() + " | W " + menuItem.getPrice() + " | " + menuItem.getDetails());
+        }
+        System.out.println("0. 종료            | 종료   |");
+        System.out.println("숫자를 입력하세요");
+        int i = scanner.nextInt();
+        if(i > 0 && i <= menuItems.size()){
+            MenuItem menuItem = menuItems.get(i);
+            System.out.println("선택한 메뉴 : "+ menuItem.getName() + " | W " + menuItem.getPrice() + " | " + menuItem.getDetails());
+        }
+        else if(i==0){
             System.out.println("프로그램을 종료합니다.");
         }
-
-
+        else{
+            System.out.println("잘못 입력했습니다.");
+        }
     }
 }
