030df97780eb112e172ba202f7b3d2c0f9b566b8
kimminsung <shiwing15@naver.com>
2025-04-25 23:42:35 +0900

    feat(Kiosk) 키오스크 main, 카테고리별 메뉴 출력 기능 구현

--- src/com/example/kiosk/Kiosk.java
+++ src/com/example/kiosk/Kiosk.java
@@ -1,37 +1,80 @@
 package com.example.kiosk;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Kiosk {
+    private Menu menu;
     private List<MenuItem> menuItems = new ArrayList<>();
-
-    public Kiosk(List<MenuItem> menuItems){
-        this.menuItems = menuItems;
+    private List<String> categories = new ArrayList<>();
+    private List<Menu> menuList = new ArrayList<>();
+    public Kiosk(List<Menu> menuList){
+        this.menuList = menuList;
     }
 
     public void start(){
         Scanner scanner = new Scanner(System.in);
+        int menuNum, menuItemNum;
+        Menu menu;
+        MenuItem menuItem;
+
         while(true){
-            System.out.println("[ SHAKESHACK MENU ]");
-            for (int i = 0; i < menuItems.size(); i++){
-                MenuItem menuItem = menuItems.get(i);
-                System.out.println(i+1 + "." + menuItem.getName() + " | W " + menuItem.getPrice() + " | " + menuItem.getDetails());
+            System.out.println("[ MAIN MENU ]");
+            for(int i = 0; i<menuList.size(); i++){
+                System.out.println((i+1) + " "+ menuList.get(i).getCategories());
             }
-            System.out.println("0. 종료            | 종료   |");
-            System.out.println("숫자를 입력하세요");
-            int i = scanner.nextInt();
-            if(i > 0 && i <= menuItems.size()){
-                MenuItem menuItem = menuItems.get(i-1);
-                System.out.println("선택한 메뉴 : "+ menuItem.getName() + " | W " + menuItem.getPrice() + " | " + menuItem.getDetails());
+            System.out.println("0. 종료");
+
+            try{
+                menuNum = scanner.nextInt();
+            } catch(InputMismatchException e){
+                System.out.println("숫자만 입력해주세요.");
+                scanner.nextLine();
+                continue;
             }
-            else if(i==0){
+
+            if(menuNum == 1 || menuNum ==2 || menuNum == 3){
+                System.out.println("[ Burgers Menu ]");
+                menu = menuList.get(menuNum-1);
+                menuItems = menu.getMenuItems();
+                for(int i = 0; i<menuItems.size();i++){
+                    System.out.println((i+1) + ". "+menuItems.get(i).getName()+ " | W " + menuItems.get(i).getPrice()
+                            + " | " + menuItems.get(i).getDetails());
+                }
+                System.out.println("0. 뒤로가기");
+
+                while(true){
+                    try{
+                        menuItemNum = scanner.nextInt();
+                    } catch(InputMismatchException e){
+                        System.out.println("숫자만 입력해주세요.");
+                        scanner.nextLine();
+                        continue;
+                    }
+                    if(menuItemNum>=1 && menuItemNum <= menuItems.size()){
+                        menuItem = menuItems.get(menuItemNum-1);
+                        System.out.print("선택한 메뉴 : ");
+                        System.out.println(menuItem.getName()+ " | W " + menuItem.getPrice() + " | " + menuItem.getDetails());
+                    }
+                    else if(menuItemNum == 0 ){
+                        System.out.println("메인 메뉴로 돌아갑니다.");
+                    }
+                    else{
+                        System.out.println("잘못된 입력입니다.");
+                        continue;
+                    }
+                    break;
+                }
+            }
+            else if(menuNum == 0){
                 System.out.println("프로그램을 종료합니다.");
-                break;
+                return;
             }
             else{
-                System.out.println("잘못 입력했습니다.");
+                System.out.println("잘못된 입력입니다.");
+                continue;
             }
         }
     }
